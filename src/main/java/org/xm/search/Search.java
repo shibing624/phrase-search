c30b7fedd813681b8ffd9c8b6fec10682ab9ebd7
xuming <507153809@qq.com>
2017-06-15 16:33:57 +0800

    初始构建phrase-search项目。xuming 20170615

---
+++ src/main/java/org/xm/search/Search.java
@@ -0,0 +1,21 @@
+package org.xm.search;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Search:搜索服务工具包
+ *
+ * @author XuMing
+ */
+public class Search {
+
+    /**
+     * 日志组件
+     */
+    private static Logger logger = LogManager.getLogger();
+
+    public static void main(String[] args) {
+        logger.debug("debug");
+    }
+}
