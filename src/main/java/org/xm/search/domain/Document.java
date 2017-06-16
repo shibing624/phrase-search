2720432c789b31c6ab816567dacdd10091189841
xuming <507153809@qq.com>
2017-06-16 10:09:53 +0800

    搜索文档。xuming 20170615

--- src/main/java/org/xm/search/domain/Document.java
+++ src/main/java/org/xm/search/domain/Document.java
@@ -13,12 +13,81 @@ public class Document implements Comparable {
     private String value;
     private Set<String> terms = new HashSet<>();
     private int score;
-    public Document(){}
-    public Document(int id,String value){
 
+    public Document() {
     }
+
+    public Document(int id, String value) {
+        this.id = id;
+        this.value = value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Set<String> getTerms() {
+        return terms;
+    }
+
+    public void setTerms(Set<String> terms) {
+        this.terms = terms;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public Document clone() {
+        Document document = new Document();
+        document.id = id;
+        document.value = value;
+        document.terms = terms;
+        document.score = score;
+        return document;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Document document = (Document) o;
+        return id == document.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id ^ (id >>> 32);
+    }
+
+    @Override
+    public String toString() {
+        return "Document{" +
+                "id=" + id +
+                ", value='" + value + '\'' +
+                ", terms=" + terms +
+                ", score=" + score +
+                "}";
+    }
+
     @Override
     public int compareTo(Object o) {
-        return 0;
+        return Integer.valueOf(id).compareTo(((Document) o).getId());
     }
 }
